e5730da9c9e793aa2c17729f6a7495e52ceedb0d
lakyus <lakyus@localhost.localdomain>
2021-05-03 09:47:32 +0200

    Changed behaviour of loop

--- exam/tri.c
+++ exam/tri.c
@@ -2,29 +2,31 @@
 
 int tri(int array[], int size) {
   int c;
-  for (int i = 0, n = size; i < size; i++, n--) {
-    //On regarde si le caractère actuel est plus grand que le prochain
-    //Si oui, on les échange
-    if (array[i] > array[i+1]) {
-      c = array[i+1];
-      array[i+1] = array[i];
-      array[i] = c;
-    }
+  for (int i = 0; i < size; i++) {
+    for (int i = 0, n = size; i < size; i++, n--) {
+      //On regarde si le caractère actuel est plus grand que le prochain
+      //Si oui, on les échange
+      if (array[i] > array[i+1]) {
+	c = array[i+1];
+	array[i+1] = array[i];
+	array[i] = c;
+      }
     //On regarde si le caractère actuel est plus petit que le caractère d'avant
     //Si oui on les échange
-    if (array[i] < array[i-1]) {
-      c = array[i-1];
-      array[i-1] = array[i];
-      array[i] = c;
-    }
-    /* On regarde à partir de la fin de la chaine, si le caractère actuel
+      if (array[i] < array[i-1]) {
+	c = array[i-1];
+	array[i-1] = array[i];
+	array[i] = c;
+      }
+      /* On regarde à partir de la fin de la chaine, si le caractère actuel
        est plus petit que le caractère d'avant, et on parcours la chaine
        à l'envers pour continuer l'opération inverse
-    */
-    if (array[n] < array[n-1]) {
-      c = array[n];
-      array[n] = array[n-1];
-      array[n-1] = c;
+      */
+      if (array[n] < array[n-1]) {
+	c = array[n];
+	array[n] = array[n-1];
+	array[n-1] = c;
+      }
     }
   }
   //On affiche chaque caractère dans le tableau après modifications
@@ -36,9 +38,9 @@ int tri(int array[], int size) {
 }
 
 int main(void) {
-  int array[] = {12, 31, 5, 42, 10};
+  int array[] = {12, 31, 5, 42, 10, 50, 25, 2, 8};
 
   //On exécute la fonction
-  tri(array, 5);
+  tri(array, 9);
   return 0;
 }
