9ff1854daa3e21850520c5fde76f45c5c4cc16df
lakyus <lakyus@localhost.localdomain>
2021-05-05 12:06:11 +0200

    Updated ex4

--- final_exam/4transpose.c
+++ final_exam/4transpose.c
@@ -2,18 +2,30 @@
 #include<stdlib.h>
 
 void transpose(int array[3][3]) {
+  int temp;
+  
   for (int i = 0; i < 3; i++) {
+    for (int n = 0; n < i; n++) {
+      temp = array[i][n];
+      array[i][n] = array[n][i];
+      array[n][i] = temp;
+    }
+    for (int n = 2; n >= 0; n--) {
+      temp = array[i][n];
+      array[i][n] = array[n][i];
+      array[n][i] = temp;
+    }
     for (int n = 0; n < 3; n++) {
-      printf("%d ", array[n][i]);
+      printf("%d ", array[i][n]);
     }
     printf("\n");
   }
 }
 
 int main() {
-    int tableau1[3][3] = { {1, 2, 3},
-                           {4, 5, 6},
-                           {7, 8, 9} };
+  int tableau1[3][3] = { {1, 2, 3},
+			 {4, 5, 6},
+			 {7, 8, 9} };
     int i,j;
 
     for (i=0; i<3; i++) {
